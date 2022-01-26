6421cb70da78bb3de34682dec8ba77012c6e7df9
Olio72AT <github@erfolgsmanufaktur.com>
2022-01-26 21:46:02 +0100

    Projektdateien hinzufügen.

---
+++ TravelApp/TravelApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            LoginButton.Text = "Peter";
+            bool EmailEmpty = string.IsNullOrEmpty(EmailEntry.Text);
+            bool PasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+
+            if (EmailEmpty || PasswordEmpty)
+            {
+                // Do not navigate
+            }
+            else
+            {
+                // Navigate
+                Navigation.PushAsync(new HomePage());
+
+            }
+        }
+    }
+}
